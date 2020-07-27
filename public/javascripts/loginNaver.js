280dea2abc3e2c4947146e79487d44222da43046
Yeongil Yoon <askme143@kaist.ac.kr>
2020-07-27 14:33:01 +0000

    Pass email and name to signupNaver page

--- public/javascripts/loginNaver.js
+++ public/javascripts/loginNaver.js
@@ -1,15 +1,16 @@
 function naverSignInCallback(naver_id_login) {
     console.log(naver_id_login);
-    $.get("http://192.249.19.242:7380/login"
+    $.get("http://192.249.19.244:1580/login"
             + "?email=" + naver_id_login.getProfileData('email'), (data) => {
         console.log(data);
         if (data == "success") {
             window.opener.location = naver_id_login.redirect_uri;
             window.close();
         } else {
-            /* Show error message */
-            /* sign up page */
-            // window.location = 
+            window.opener.location = "http://192.249.19.244:1580/signupNaver.html"
+                                        + "?email=" + naver_id_login.getProfileData('email')
+                                        + "&name=" + naver_id_login.getProfileData('name');
+            window.close();
         }
     })
 }
