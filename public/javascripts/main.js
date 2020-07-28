02a96384609c036a5d67a922fbedf589faef47cc
Yeongil Yoon <askme143@kaist.ac.kr>
2020-07-28 17:41:04 +0000

    Fix mobile profile

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,44 @@
+function isMobile(){
+	var UserAgent = navigator.userAgent;
+
+    if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null
+     || UserAgent.match(/LG|SAMSUNG|Samsung/) != null) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 function loginCheck() {
     $.get("/loginCheck", (data) => {
         if (data == "success") {
-            const profileBtn = document.querySelector("#login_btn a");
-            const logoutBtn = document.querySelector("#signup_btn a");
+            var profileBtn = document.querySelector("#login_btn a");
+            var logoutBtn = document.querySelector("#signup_btn a");
+            const mobile = isMobile();
 
-            profileBtn.innerHTML = "프로필";
-            profileBtn.setAttribute('href', "#");
+            if (!mobile) {
+                profileBtn.innerHTML = "프로필";
+                profileBtn.setAttribute('href', "#");
+                logoutBtn.innerHTML = "로그아웃";
+            } else {
+                logoutBtn = profileBtn;
+            }
 
-            logoutBtn.innerHTML = "로그아웃";
             logoutBtn.setAttribute('href', "#");
             logoutBtn.addEventListener('click', (ev) => {
                 const loginBtn = document.querySelector("#login_btn a");
                 const signupBtn = document.querySelector("#signup_btn a");
+                const mobile = isMobile();
 
-                loginBtn.innerHTML = "로그인";
                 loginBtn.setAttribute('href', "login.html");
+                if (!mobile) {
+                    loginBtn.innerHTML = "로그인";
+                    signupBtn.innerHTML = "회원가입";
+                }
 
-                signupBtn.innerHTML = "회원가입";
                 $.get("/logout", (data) => {
-                    signupBtn.setAttribute('href', "signup.html");
+                    if (!mobile)
+                        signupBtn.setAttribute('href', "signup.html");
                 })
             })
         } else {
